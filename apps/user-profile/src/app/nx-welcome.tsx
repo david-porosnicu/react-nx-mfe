62dbad924c399bf775a56545990a7c42ca38b6c3
david-porosnicu <david.porosnicu@gmail.com>
2025-05-14 21:54:55 +0300

    changes to both

--- apps/user-profile/src/app/nx-welcome.tsx
+++ apps/user-profile/src/app/nx-welcome.tsx
@@ -431,7 +431,7 @@ export function NxWelcome({ title }: { title: string }) {
         <div className="container">
           <div id="welcome">
             <h1>
-              <span> Hello there, </span>
+              <span> Hello there TEST_TEST, </span>
               Welcome {title} 👋
             </h1>
           </div>
