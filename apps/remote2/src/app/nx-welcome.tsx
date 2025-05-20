756d8391031c23de1a6ec2ab53397221072103e6
david-porosnicu <david.porosnicu@gmail.com>
2025-05-20 14:19:49 +0300

    change in remote 2 test

--- apps/remote2/src/app/nx-welcome.tsx
+++ apps/remote2/src/app/nx-welcome.tsx
@@ -431,7 +431,7 @@ export function NxWelcome({ title }: { title: string }) {
         <div className="container">
           <div id="welcome">
             <h1>
-              <span> Hello there, </span>
+              <span> Hello there, TEST IN REMOTE 2 </span>
               Welcome {title} 👋
             </h1>
           </div>
