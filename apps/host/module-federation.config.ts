77f488c90d2947b4b9dc0b448e07ecf15ef7f249
david-porosnicu <david.porosnicu@gmail.com>
2025-05-20 14:04:50 +0300

    add remote 1

--- apps/host/module-federation.config.ts
+++ apps/host/module-federation.config.ts
@@ -14,7 +14,7 @@ const config: ModuleFederationConfig = {
    * declare module 'my-external-remote';
    *
    */
-  remotes: ['user-profile'],
+  remotes: ['user-profile', 'remote1'],
 };
 
 export default config;
--- apps/host/src/app/app.tsx
+++ apps/host/src/app/app.tsx
@@ -4,6 +4,8 @@ import NxWelcome from './nx-welcome';
 
 import { Link, Route, Routes } from 'react-router-dom';
 
+const Remote1 = React.lazy(() => import('remote1/Module'));
+
 const UserProfile = React.lazy(() => import('user-profile/Module'));
 
 export function App() {
@@ -14,11 +16,15 @@ export function App() {
           <Link to="/">Home</Link>
         </li>
         <li>
+          <Link to="/remote1">Remote1</Link>
+        </li>
+        <li>
           <Link to="/user-profile">UserProfile</Link>
         </li>
       </ul>
       <Routes>
         <Route path="/" element={<NxWelcome title="host" />} />
+        <Route path="/remote1" element={<Remote1 />} />
         <Route path="/user-profile" element={<UserProfile />} />
       </Routes>
     </React.Suspense>
